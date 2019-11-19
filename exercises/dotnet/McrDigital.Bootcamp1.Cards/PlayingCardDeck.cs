07feb5743bb714b848a20574abf5ba00450de04f
Duncan Joyce <Duncan_Joyce@waters.com>
2019-11-19 10:41:48 +0000

    Snap working for playing cards

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/PlayingCardDeck.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Cards
+{
+    class PlayingCardDeck : Deck
+    {
+        public PlayingCardDeck()
+        {
+            Cards = new List<GenericCard>();
+
+            var suits = new Suit[]{
+                new Suit("clubs"),
+                new Suit("diamonds"),
+                new Suit("hearts"),
+                new Suit("spades") };
+
+            var faceValues = new FaceValue[]
+            {
+             new FaceValue("ace"),
+             new FaceValue("2"),
+             new FaceValue("3"),
+             new FaceValue("4"),
+             new FaceValue("5"),
+             new FaceValue("6"),
+             new FaceValue("7"),
+             new FaceValue("8"),
+             new FaceValue("9"),
+             new FaceValue("10"),
+             new FaceValue("jack"),
+             new FaceValue("queen"),
+             new FaceValue("king")
+
+        };
+
+            foreach (var suit in suits)
+            {
+                foreach (var faceValue in faceValues)
+                {
+                    var card = new Card(suit, faceValue);
+                    Cards.Add(card);
+                }
+            }
+        }
+
+        public override string[] GetCards()
+        {
+            var result = new String[52];
+
+            var suits = new Suit[]{
+                new Suit("clubs"),
+                new Suit("diamonds"),
+                new Suit("hearts"),
+                new Suit("spades") };
+
+            var faceValues = new FaceValue[]
+            {
+             new FaceValue("ace"),
+             new FaceValue("2"),
+             new FaceValue("3"),
+             new FaceValue("4"),
+             new FaceValue("5"),
+             new FaceValue("6"),
+             new FaceValue("7"),
+             new FaceValue("8"),
+             new FaceValue("9"),
+             new FaceValue("10"),
+             new FaceValue("jack"),
+             new FaceValue("queen"),
+             new FaceValue("king")
+
+        };
+
+            var deck = new List<Card>();
+
+            foreach (var suit in suits)
+            {
+                foreach (var faceValue in faceValues)
+                {
+                    var card = new Card(suit, faceValue);
+                    deck.Add(card);
+                }
+            }
+            var cardNumber = 0;
+            foreach (var card in deck)
+            {
+                result[cardNumber] = $"{card.value.valueName}";
+                cardNumber++;
+            }
+            return result;
+        }
+
+        public override void Shuffle()
+        {
+            this.Cards.KnuthShuffle();
+        }
+    }
+
+}
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/PlayingCardSnap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Cards
+{
+    public class PlayingCardSnap
+    {
+        static void Main(string[] args)
+        {
+            var snap = new PlayingCardSnap(new PlayingCardDeck());
+            snap.Play();
+        }
+
+        private int _player1Score;
+        private int _player2Score;
+        private readonly Deck _deck;
+
+        public PlayingCardSnap(Deck deck)
+        {
+            this._deck = deck;
+            this._deck.Shuffle();
+        }
+
+        public void Play()
+        {
+            GenericCard previousCard = null;
+            while (this._deck.GetCards().Length > 0)
+            {
+                var currentCard = this._deck.Deal();
+                Console.WriteLine(currentCard.CardValue );
+
+                var input = Console.ReadLine();
+                if (input.Length > 0 && input[0] == 'a')
+                {
+                    if (currentCard.Snap(previousCard))
+                    {
+                        Console.WriteLine("SNAP! score Player 1");
+                        this._player1Score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("WRONG! deducting score from Player 1");
+                        this._player1Score--;
+                    }
+                }
+                else if (input.Length > 0 && input[0] == 'l')
+                {
+                    if (currentCard.Snap(previousCard))
+                    {
+                        Console.WriteLine("SNAP! score Player 2");
+                        this._player2Score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("WRONG! deducting score from Player 2");
+                        this._player2Score--;
+                    }
+                }
+                previousCard = currentCard;
+            }
+
+            if (this._player1Score == this._player2Score)
+            {
+                Console.WriteLine("Draw.");
+            }
+            else if (this._player1Score > this._player2Score)
+            {
+                Console.WriteLine("Player 1 wins!");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 wins!");
+            }
+
+            Console.WriteLine($"Scores: {this._player1Score} vs {this._player2Score}");
+        }
+    }
+}
